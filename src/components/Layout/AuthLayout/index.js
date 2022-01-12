9488126405fdd9b7d22c13314191fdfb24e86872
farhan <farhanmahesa10@gmail.com>
2022-01-12 14:53:27 +0700

    adding footer

--- src/components/Layout/AuthLayout/index.js
+++ src/components/Layout/AuthLayout/index.js
@@ -3,18 +3,17 @@ import Footer from "./Footer";
 import Header from "./Header";
 const AuthLayout = (props) => {
   return (
-    <div className="d-flex flex-column h-screen ">
-      <Header />
-      <div className="d-sm-flex h-full w-full  justify-content-center align-items-center">
-        <main class="flex-shrink-0 mb-4">
-          <div class="container h-full ">
-            <div className="d-sm-flex  justify-content-center align-items-center text-center">
-              {props.children}
-            </div>
-          </div>
-        </main>
+    <div className="d-flex flex-column justify-content-between h-screen">
+      <div>
+        <Header />
       </div>
-      <Footer />
+
+      <div className="d-sm-flex container justify-content-center align-items-center text-center">
+        {props.children}
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 };
