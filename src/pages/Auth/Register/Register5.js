1e253bec1d58295d0509e6215e607f35176312d2
farhan <farhanmahesa10@gmail.com>
2022-01-27 09:31:17 +0700

    gmainl

--- src/pages/Auth/Register/Register5.js
+++ src/pages/Auth/Register/Register5.js
@@ -4,29 +4,26 @@ import Header from "../../../components/Layout/AuthLayout/Header";
 const Register5 = () => {
   return (
     <>
-      <Header />
-      <div className="h-screen p-4 ">
-        <div
-          className="w-full  h-full d-flex justify-content-center align-items-center"
-          style={{ marginTop: "-100px" }}
-        >
-          <div className="p-4 text-center" style={{ width: "550px" }}>
-            <div className="text-center mb-52">
-              <img src="/images/gmail.png" alt="" />
-            </div>
-            <h2 className="mb-8">Please check your email</h2>
-            <div className="d-flex align-items-center gap-28 align-items">
-              <div>
-                <p className="mb-4">
-                  Please check your email to confirm the registration email we
-                  sent. Haven’t received an email? Please check your spam folder
-                  to make sure it’s not in there.
-                </p>
-              </div>
+      <div className="h-screen d-flex justify-content-between align-items-center flex-column">
+        <div className="w-full">
+          <Header />
+        </div>
+        <div className="w-500 text-center">
+          <div className="text-center mb-52">
+            <img src="/images/gmail.png" alt="" />
+          </div>
+          <h2 className="mb-8">Please check your email</h2>
+          <div className="d-flex align-items-center  align-items">
+            <div>
+              <p className="mb-4">
+                Please check your email to confirm the registration email we
+                sent. Haven’t received an email? Please check your spam folder
+                to make sure it’s not in there.
+              </p>
             </div>
           </div>
         </div>
-        <div className="text-center">
+        <div className="text-center pb-64">
           <Link to="" className=" text-black ">
             Resend confirmation password (18)
           </Link>
