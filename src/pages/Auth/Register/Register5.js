c4e714a8d6beca9cca3712c78079cdf9fcf4ea11
farhan <farhanmahesa10@gmail.com>
2022-01-27 09:22:41 +0700

    gmainl

--- src/pages/Auth/Register/Register5.js
+++ src/pages/Auth/Register/Register5.js
@@ -1,41 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Header from "../../../components/Layout/AuthLayout/Header";
 const Register5 = () => {
   return (
-    <div className="h-screen p-4 ">
-      <div className="row justify-content-center w-full">
-        <div className="d-flex justify-content-center ">
-          <div>
-            <img src="/images/logo.png" className="img-fluid" alt="..." />
-          </div>
-        </div>
-      </div>
-      <div
-        className="w-full  h-full d-flex justify-content-center align-items-center"
-        style={{ marginTop: "-100px" }}
-      >
-        <div className="p-4 text-center" style={{ width: "550px" }}>
-          <div className="text-center mb-52">
-            <img src="/images/gmail.png" alt="" />
-          </div>
-          <h2 className="mb-8">Please check your email</h2>
-          <div className="d-flex align-items-center gap-28 align-items">
-            <div>
-              <p className="mb-4">
-                Please check your email to confirm the registration email we
-                sent. Haven’t received an email? Please check your spam folder
-                to make sure it’s not in there.
-              </p>
+    <>
+      <Header />
+      <div className="h-screen p-4 ">
+        <div
+          className="w-full  h-full d-flex justify-content-center align-items-center"
+          style={{ marginTop: "-100px" }}
+        >
+          <div className="p-4 text-center" style={{ width: "550px" }}>
+            <div className="text-center mb-52">
+              <img src="/images/gmail.png" alt="" />
+            </div>
+            <h2 className="mb-8">Please check your email</h2>
+            <div className="d-flex align-items-center gap-28 align-items">
+              <div>
+                <p className="mb-4">
+                  Please check your email to confirm the registration email we
+                  sent. Haven’t received an email? Please check your spam folder
+                  to make sure it’s not in there.
+                </p>
+              </div>
             </div>
           </div>
         </div>
+        <div className="text-center">
+          <Link to="" className=" text-black ">
+            Resend confirmation password (18)
+          </Link>
+        </div>
       </div>
-      <div className="text-center">
-        <Link to="" className=" text-black ">
-          Resend confirmation password (18)
-        </Link>
-      </div>
-    </div>
+    </>
   );
 };
 
