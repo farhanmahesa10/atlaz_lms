aedcd5ea000437b83fdded7d38090db72016d099
farhan <farhanmahesa10@gmail.com>
2022-02-04 11:16:50 +0700

    asdadas

--- src/pages/Auth/Register/Register2.js
+++ src/pages/Auth/Register/Register2.js
@@ -49,7 +49,7 @@ const Register2 = () => {
           </div>
           <div className="d-grid grid-cols-2 gap-3  ">
             <Link to="/register" className="  ">
-              <button className="text-center btn-outline"> Back</button>
+              <button className="text-center btn-outline w-full"> Back</button>
             </Link>
             <button
               type={`${allowNext ? "submit" : "button"}`}
