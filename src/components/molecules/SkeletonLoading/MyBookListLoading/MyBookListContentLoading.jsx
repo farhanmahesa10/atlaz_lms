f94fa40af0a5af07ab5c7dc800a4c2daf3592ea3
M Farhan <farhanmahesa10@gmail.com>
2022-07-05 13:58:57 +0700

    ready to build

--- src/components/molecules/SkeletonLoading/MyBookListLoading/MyBookListContentLoading.jsx
+++ src/components/molecules/SkeletonLoading/MyBookListLoading/MyBookListContentLoading.jsx
@@ -19,7 +19,7 @@ const MyBookListContentLoading = (props) => {
         {count.map((r, i) => {
           return (
             <div key={`load-${i}`} className="col-6 col-sm-4 col-lg-3  mb-24">
-              <div className="max-w-127 md-max-w-188 h-279">
+              <div className="max-w-127 md-max-w-188 h-200">
                 <Skeleton height={"100%"} width="100%" />
               </div>
             </div>
