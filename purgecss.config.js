d9fce63e3b7fbaaeba9e3c7629a85098a5d27216
M Farhan <farhanmahesa10@gmail.com>
2022-07-26 13:03:58 +0700

    css fixed

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: ["./public/index.html", "./src/**/*.jsx", "./src/**/*.js"],
   css: ["./src/fake-style.css"],
+  safelist: ["form-input-error", "yep", "button"],
   // output: "/result",
 };
