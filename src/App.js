dc29f564eaf6bcb9cdbb86ede9fd50925ab17b81
Atlaz 5 <atlaz5@Atlaz-5s-MacBook-Pro.local>
2022-08-01 10:52:30 +0700

    add react GA

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./assets/css/App.css";
 import Routing from "./config/Routing";
+import ReactGA from 'react-ga';
+const GA_ID = process.env.REACT_APP_ID_GA
+
 
 function App() {
+  ReactGA.initialize(GA_ID);
+  ReactGA.pageview(window.location.pathname + window.location.search);
+  console.log(GA_ID)
   return (
     <>
       <Routing />
